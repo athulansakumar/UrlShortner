1481dd2b1fd8393c66db8e9c64ee73cdaee6469b
Athul Ansakumar <athul.ansakumar@gmail.com>
2018-06-07 23:20:10 +0530

    Correcting Missed packagename

--- src/main/java/com/athulansakumar/web/UrlShortnerApplication.java
+++ src/main/java/com/athulansakumar/web/UrlShortnerApplication.java
@@ -1,4 +1,4 @@
-package com.athulansakumar.application;
+package com.athulansakumar.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
